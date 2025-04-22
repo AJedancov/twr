dc2fb6b53093ce50a436f151eb3393ac15528ab2
AJedancov <andrei.jedancov@gmail.com>
2025-04-22 14:01:42 +0300

    feat(launch): add launch arguments to specify path to nav2 parameter files

--- twr_navigation/launch/navigation.launch.py
+++ twr_navigation/launch/navigation.launch.py
@@ -22,8 +22,30 @@ def generate_launch_description():
         description='Use simulation time',
     )
 
+    nav2_bt_params_path_launch_arg = DeclareLaunchArgument(
+        name='nav2_bt_params_path',
+        default_value=PathJoinSubstitution([twr_navigation_pkg_path, 'config', 'nav2', 'bt', 'nav2_bt_params.yaml']),
+        description='Path to Nav2 behavior tree parameters',
+    )
+
+    nav2_planner_params_path_launch_arg = DeclareLaunchArgument(
+        name='nav2_planner_params_path',
+        default_value=PathJoinSubstitution([twr_navigation_pkg_path, 'config', 'nav2', 'planner', 'nav2_planner_navfn_params.yaml']),
+        description='Path to Nav2 planner parameters',
+    )
+
+    nav2_controller_params_path_launch_arg = DeclareLaunchArgument(
+        name='nav2_controller_params_path',
+        default_value=PathJoinSubstitution([twr_navigation_pkg_path, 'config', 'nav2', 'controller', 'nav2_controller_mppi_params.yaml']),
+        description='Path to Nav2 controller parameters',
+    )
+
     # === Launch configuration === 
     use_sim_time_launch_conf = LaunchConfiguration('use_sim_time')
+    nav2_bt_params_path_launch_conf = LaunchConfiguration('nav2_bt_params_path')
+    nav2_planner_params_path_launch_conf = LaunchConfiguration('nav2_planner_params_path')
+    nav2_controller_params_path_launch_conf = LaunchConfiguration('nav2_controller_params_path')
+
 
     # ============
     # === Nav2 ===
@@ -33,7 +55,7 @@ def generate_launch_description():
     
     # === Behaviour Tree Server ===
     nav2_bt_params = [
-        PathJoinSubstitution([twr_navigation_pkg_path, 'config', 'nav2_bt.yaml']),
+        nav2_bt_params_path_launch_conf,
         {'use_sim_time': use_sim_time_launch_conf,}
     ]
 
@@ -58,7 +80,7 @@ def generate_launch_description():
     
     # === Planner Server ===
     nav2_planner_server_params = [
-        PathJoinSubstitution([twr_navigation_pkg_path, 'config', 'nav2_planner.yaml']),
+        nav2_planner_params_path_launch_conf,
         {'use_sim_time': use_sim_time_launch_conf,}
     ]
 
@@ -73,7 +95,7 @@ def generate_launch_description():
 
     # === Controller Server ===
     nav2_controller_server_params = [
-        PathJoinSubstitution([twr_navigation_pkg_path, 'config', 'nav2_controller_mppi.yaml']),
+        nav2_controller_params_path_launch_conf,
         {'use_sim_time': use_sim_time_launch_conf,}
     ]
     
@@ -96,7 +118,6 @@ def generate_launch_description():
     nav2_lifecycle_manager_params = [
         {'autostart': True,
         'node_names': nav2_lifecycle_nodes,}
-
     ]
 
     nav2_lifecycle_manager_node = Node(
@@ -106,6 +127,11 @@ def generate_launch_description():
         output='screen',
         parameters=nav2_lifecycle_manager_params,
     )
+
+    ld.add_action(use_sim_time_launch_arg)
+    ld.add_action(nav2_bt_params_path_launch_arg)
+    ld.add_action(nav2_planner_params_path_launch_arg)
+    ld.add_action(nav2_controller_params_path_launch_arg)
 
     ld.add_action(nav2_behavior_server_node)
     ld.add_action(nav2_bt_navigator_node)
