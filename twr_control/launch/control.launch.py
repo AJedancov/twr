3bc8f9a60f065675c8137c2422cc49d96fc00e38
AJedancov <andrei.jedancov@gmail.com>
2025-08-15 15:43:23 +0300

    refactor: replace nav2 behavior trees management in twr_control section

--- twr_control/launch/control.launch.py
+++ twr_control/launch/control.launch.py
@@ -24,6 +24,28 @@ def generate_launch_description():
         description='Use simulation time',
     )
     
+    nav2_bt_params_path_launch_arg = DeclareLaunchArgument(
+        name='nav2_bt_params_path',
+        default_value=PathJoinSubstitution([
+            twr_control_pkg_path, 
+            'nav2_bt',
+            'config', 
+            'nav2_bt_params.yaml'
+        ]),
+        description='Path to Nav2 behavior tree parameters',
+    )
+
+    nav2_nav_to_pose_bt_path_launch_arg = DeclareLaunchArgument(
+        name='nav2_nav_to_pose_bt_path',
+        default_value=PathJoinSubstitution([
+            twr_control_pkg_path, 
+            'nav2_bt',
+            'behavior_trees',
+            'navigate_to_pose.xml'
+        ]),
+        description='Path to behavior tree for navigate_to_pose',
+    )
+    
     nav2_controller_server_params_path_launch_arg = DeclareLaunchArgument(
         name='nav2_controller_server_params_path',
         default_value=PathJoinSubstitution([
@@ -49,6 +71,8 @@ def generate_launch_description():
 
     # === Launch configuration ===
     use_sim_time_launch_conf = LaunchConfiguration('use_sim_time')
+    nav2_bt_params_path_launch_conf = LaunchConfiguration('nav2_bt_params_path')
+    nav2_nav_to_pose_bt_path_launch_conf = LaunchConfiguration('nav2_nav_to_pose_bt_path')
     nav2_controller_server_params_path_launch_conf = LaunchConfiguration('nav2_controller_server_params_path')
     nav2_local_costmap_params_path_launch_conf = LaunchConfiguration('nav2_local_costmap_params_path')
 
@@ -89,13 +113,40 @@ def generate_launch_description():
         arguments=spawner_node_args,
     )
 
-    # ====================
-    # === Nav2 Control ===
-    # ====================
+
+    # ============
+    # === Nav2 ===
+    # ============
 
     common_remappings = [('/tf', 'tf'), ('/tf_static', 'tf_static')]
     cmd_vel_remappings = [('cmd_vel', 'diff_drive_controller/cmd_vel')]
     
+    # === Behaviour Tree Server ===
+    nav2_bt_params = [
+        nav2_bt_params_path_launch_conf,
+        {'use_sim_time': use_sim_time_launch_conf,
+        'default_nav_to_pose_bt_xml': nav2_nav_to_pose_bt_path_launch_conf}
+    ]
+
+    nav2_behavior_server_node = Node(
+        package='nav2_behaviors',
+        executable='behavior_server',
+        name='behavior_server',
+        output='screen',
+        parameters=nav2_bt_params,
+        remappings=common_remappings + cmd_vel_remappings,
+    )
+
+    # === Behaviour Tree Navigator ===
+    nav2_bt_navigator_node = Node(
+        package='nav2_bt_navigator',
+        executable='bt_navigator',
+        name='bt_navigator',
+        output='screen',
+        parameters=nav2_bt_params,
+        remappings=common_remappings,
+    )
+
     # === Controller Server ===
     nav2_controller_server_params = [
         nav2_controller_server_params_path_launch_conf,
@@ -112,10 +163,14 @@ def generate_launch_description():
     )
 
     ld.add_action(use_sim_time_launch_arg)
+    ld.add_action(nav2_bt_params_path_launch_arg)
+    ld.add_action(nav2_nav_to_pose_bt_path_launch_arg)
     ld.add_action(nav2_controller_server_params_path_launch_arg)
     ld.add_action(nav2_local_costmap_params_path_launch_arg)
 
     ld.add_action(spawner_node)
+    ld.add_action(nav2_behavior_server_node)
+    ld.add_action(nav2_bt_navigator_node)
     ld.add_action(nav2_controller_server_node)
     
     return ld
--- twr_navigation/launch/navigation.launch.py
+++ twr_navigation/launch/navigation.launch.py
@@ -21,28 +21,6 @@ def generate_launch_description():
         description='Use simulation time',
     )
 
-    nav2_nav_to_pose_bt_path_launch_arg = DeclareLaunchArgument(
-        name='nav2_nav_to_pose_bt_path',
-        default_value=PathJoinSubstitution([
-            twr_navigation_pkg_path, 
-            'nav2_bt',
-            'behavior_trees',
-            'navigate_to_pose.xml'
-        ]),
-        description='Path to behavior tree for navigate_to_pose',
-    )
-
-    nav2_bt_params_path_launch_arg = DeclareLaunchArgument(
-        name='nav2_bt_params_path',
-        default_value=PathJoinSubstitution([
-            twr_navigation_pkg_path, 
-            'nav2_bt',
-            'config', 
-            'nav2_bt_params.yaml'
-        ]),
-        description='Path to Nav2 behavior tree parameters',
-    )
-
     nav2_planner_server_params_path_launch_arg = DeclareLaunchArgument(
         name='nav2_planner_server_params_path',
         default_value=PathJoinSubstitution([
@@ -68,8 +46,6 @@ def generate_launch_description():
 
     # === Launch configuration === 
     use_sim_time_launch_conf = LaunchConfiguration('use_sim_time')
-    nav2_nav_to_pose_bt_path_launch_conf = LaunchConfiguration('nav2_nav_to_pose_bt_path')
-    nav2_bt_params_path_launch_conf = LaunchConfiguration('nav2_bt_params_path')
     nav2_planner_server_params_path_launch_conf = LaunchConfiguration('nav2_planner_server_params_path')
     nav2_global_costmap_params_path_launch_conf = LaunchConfiguration('nav2_global_costmap_params_path')
 
@@ -77,35 +53,10 @@ def generate_launch_description():
     # ============
     # === Nav2 ===
     # ============
+
     common_remappings = [('/tf', 'tf'), ('/tf_static', 'tf_static')]
     cmd_vel_remappings = [('cmd_vel', 'diff_drive_controller/cmd_vel')]
-    
-    # === Behaviour Tree Server ===
-    nav2_bt_params = [
-        nav2_bt_params_path_launch_conf,
-        {'use_sim_time': use_sim_time_launch_conf,
-        'default_nav_to_pose_bt_xml': nav2_nav_to_pose_bt_path_launch_conf}
-    ]
-
-    nav2_behavior_server_node = Node(
-        package='nav2_behaviors',
-        executable='behavior_server',
-        name='behavior_server',
-        output='screen',
-        parameters=nav2_bt_params,
-        remappings=common_remappings + cmd_vel_remappings,
-    )
-
-    # === Behaviour Tree Navigator ===
-    nav2_bt_navigator_node = Node(
-        package='nav2_bt_navigator',
-        executable='bt_navigator',
-        name='bt_navigator',
-        output='screen',
-        parameters=nav2_bt_params,
-        remappings=common_remappings,
-    )
-    
+        
     # === Planner Server ===
     nav2_planner_server_params = [
         nav2_planner_server_params_path_launch_conf,
@@ -124,13 +75,9 @@ def generate_launch_description():
 
 
     ld.add_action(use_sim_time_launch_arg)
-    ld.add_action(nav2_nav_to_pose_bt_path_launch_arg)
-    ld.add_action(nav2_bt_params_path_launch_arg)
     ld.add_action(nav2_planner_server_params_path_launch_arg)
     ld.add_action(nav2_global_costmap_params_path_launch_arg)
 
-    ld.add_action(nav2_behavior_server_node)
-    ld.add_action(nav2_bt_navigator_node)
     ld.add_action(nav2_planner_server_node)
     
     return ld
