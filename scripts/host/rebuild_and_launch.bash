05490b77798157fd36ae943039377da74f0d7d5b
AJedancov <andrei.jedancov@gmail.com>
2026-09-08 11:31:25 +0300

    refactor: simplify rebuild_and_launch script and append arguments for launch

--- scripts/host/rebuild_and_launch.bash
+++ scripts/host/rebuild_and_launch.bash
@@ -3,9 +3,4 @@
 source install/setup.bash
 colcon build
 
-if [[ "$1" = "test" ]];
-then
-  exit 0
-else
-  ros2 launch twr_bringup twr_bringup.launch.py
-fi
+ros2 launch twr_bringup twr_bringup.launch.py $@
